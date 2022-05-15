680d5bc5c3ecbdb76a3d04e465eb27c82f86524c
Tim Shenin <t.shenin@celonis.com>
2022-05-15 14:50:39 +0200

    Fixed service tests

--- src/topics/topics.service.spec.ts
+++ src/topics/topics.service.spec.ts
@@ -1,18 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TopicsService } from './topics.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Topic } from './entities/topic.schema';
+import { Model } from 'mongoose';
 
 describe('TopicsService', () => {
-  let service: TopicsService;
+    let service: TopicsService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [TopicsService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                TopicsService,
+                {
+                    provide: getModelToken(Topic.name),
+                    useValue: Model,
+                },
+            ],
+        }).compile();
 
-    service = module.get<TopicsService>(TopicsService);
-  });
+        service = module.get<TopicsService>(TopicsService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
