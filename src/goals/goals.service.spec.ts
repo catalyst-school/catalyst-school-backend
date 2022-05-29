581f1362fc8a06adf4a012473eefb6c0862ee0a0
Tim Shenin <t.shenin@celonis.com>
2022-05-29 22:43:30 +0200

    Added tests for service

--- src/goals/goals.service.spec.ts
+++ src/goals/goals.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GoalsService } from './goals.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Goal } from './entities/goal.schema';
 
 describe('GoalsService', () => {
     let service: GoalsService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [GoalsService],
+            providers: [
+                GoalsService,
+                {
+                    provide: getModelToken(Goal.name),
+                    useValue: Model,
+                },
+            ],
         }).compile();
 
         service = module.get<GoalsService>(GoalsService);
