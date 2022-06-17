32a1c5c3c3e6de46041396d6f9b00b25ed4fcf7d
Tim Shenin <t.shenin@celonis.com>
2022-06-17 23:00:54 +0200

    Added tests to auth register controller and fixed other tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,16 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { INestApplication } from '@nestjs/common';
+import { createServiceMock } from '../../test/utils/create-service-mock';
+import { createTestModule } from '../../test/utils/create-test-module';
 
 describe('UsersController', () => {
+    let app: INestApplication;
+    let server: any;
+    const serviceMock = createServiceMock({});
     let controller: UsersController;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             controllers: [UsersController],
-            providers: [UsersService],
+            providers: [{ provide: UsersService, useValue: serviceMock }],
         }).compile();
 
+        app = await createTestModule(module);
+        server = app.getHttpServer();
         controller = module.get<UsersController>(UsersController);
     });
 
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './entities/user.schema';
 
 describe('UsersService', () => {
     let service: UsersService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [UsersService],
+            providers: [
+                UsersService,
+                {
+                    provide: getModelToken(User.name),
+                    useValue: Model,
+                },
+            ],
         }).compile();
 
         service = module.get<UsersService>(UsersService);
