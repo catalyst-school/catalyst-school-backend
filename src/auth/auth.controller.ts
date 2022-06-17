d16181cd4fc5c0f18fb1016a4ead4c2dcedc63b9
Tim Shenin <t.shenin@celonis.com>
2022-06-18 00:11:18 +0200

    Added email login endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,32 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { UsersService } from '../users/users.service';
 import { UserDto } from '../users/dto/user.dto';
+import { LoginDto } from './dto/login.dto';
+import { AuthService } from './auth.service';
+import { AppError } from '../shared/models/app-error';
 
 @Controller('auth')
 @ApiTags('auth')
 export class AuthController {
-    constructor(private userService: UsersService) {}
+    constructor(private userService: UsersService, private authService: AuthService) {}
+
+    @Post('email/login')
+    @HttpCode(HttpStatus.OK)
+    async login(@Body() loginDto: LoginDto) {
+        try {
+            return await this.authService.login(loginDto);
+        } catch (e) {
+            if (e instanceof AppError) {
+                if (e.message === 'App: Unknown user')
+                    throw new HttpException(e.message, HttpStatus.NOT_FOUND);
+                else if (e.message === 'App: Invalid password')
+                    throw new HttpException(e.message, HttpStatus.UNAUTHORIZED);
+                else throw new HttpException(e.message, HttpStatus.BAD_REQUEST);
+            } else throw new HttpException(e.message, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
     @Post('email/register')
     async register(@Body() createUserDto: CreateUserDto) {
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from '../users/entities/user.schema';
 
 @Module({
-    imports: [UsersModule],
+    imports: [UsersModule, MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
     controllers: [AuthController],
     providers: [AuthService],
 })
