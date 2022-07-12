b10213a27e82d074bba01b822d6566b4c5ad9ecf
Tim Shenin <t.shenin@celonis.com>
2022-07-12 23:37:49 +0200

    Use real mailjet api

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,32 +1,69 @@
 import { Injectable } from '@nestjs/common';
-import { MailerService } from '@nestjs-modules/mailer';
-import { ConfigService } from "@nestjs/config";
+import { ConfigService } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+
+const mailJet = require('node-mailjet');
+
+dotenv.config();
 
 @Injectable()
 export class EmailService {
-    constructor(private mailerService: MailerService, private configService: ConfigService) {}
+    private mailjet;
+    constructor(private configService: ConfigService) {
+        this.mailjet = new mailJet({
+            apiKey: process.env.MJ_APIKEY_PUBLIC,
+            apiSecret: process.env.MJ_APIKEY_PRIVATE,
+        });
+    }
 
     async emailConfirmation(email: string, token: string) {
-        await this.mailerService.sendMail({
-            to: email,
-            from: 'noreply@catalyst-school.ru',
-            subject: 'Добро пожаловать в онлайн школу Катализатор',
-            template: 'email-confirmation',
-            context: {
-                confirmationLink: `${this.configService.get("HOST")}/email-confirmation?token=${token}`
-            }
+        await this.mailjet.post('send', { version: 'v3.1' }).request({
+            Messages: [
+                {
+                    From: {
+                        Email: 'no-reply@catalyst-school.ru',
+                        Name: 'Команда школы Катализатор',
+                    },
+                    To: [
+                        {
+                            Email: email,
+                        },
+                    ],
+                    Subject: 'Добро пожаловать в онлайн школу Катализатор',
+                    HTMLPart: `
+                        <h3>Добро пожаловать в онлайн школу Катализатор</h3>
+                        <p><a href='${this.configService.get(
+                            'HOST',
+                        )}/email-confirmation?token=${token}' target='_blank'>Подтвердить Email</a>
+                        </p>
+                    `,
+                },
+            ],
         });
     }
 
     async forgotPassword(email: string, token: string) {
-        await this.mailerService.sendMail({
-            to: email,
-            from: 'noreply@catalyst-school.ru',
-            subject: 'Восстановление пароля на сайте школы Катализатор',
-            template: 'forgot-password',
-            context: {
-                forgotPasswordLink: `${this.configService.get("HOST")}/reset-password?token=${token}`
-            }
+        await this.mailjet.post('send', { version: 'v3.1' }).request({
+            Messages: [
+                {
+                    From: {
+                        Email: 'noreply@catalyst-school.ru',
+                        Name: 'Команда школы Катализатор',
+                    },
+                    To: [
+                        {
+                            Email: email,
+                        },
+                    ],
+                    Subject: 'Восстановление пароля на сайте школы Катализатор',
+                    HTMLPart: `
+                        <h3>Восстановление пароля</h3>
+                        <p><a href='${this.configService.get(
+                            'HOST',
+                        )}/reset-password?token=${token}' target='_blank'>Восстановить пароль</a></p>
+                    `,
+                },
+            ],
         });
     }
 }
