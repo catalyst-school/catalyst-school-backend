1e086ac9640920a12707707c2f5654678c0c1f61
radionov <Alexander.Radionov@infowatch.com>
2023-01-07 18:31:33 +0700

    added file controller and service tests

--- src/files/files.controller.spec.ts
+++ src/files/files.controller.spec.ts
@@ -1,18 +1,65 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { FilesService } from './files.service';
+import { createTestModule } from '../../test/utils/create-test-module';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { JwtAuthGuardMock } from '../shared/guards/jwt-auth.guard.mock';
+import { JwtAuthGuard } from '../shared/guards/jwt-auth.guard';
 import { FilesController } from './files.controller';
+import { path } from 'app-root-path';
 
-describe('FilesController', () => {
-    let controller: FilesController;
+describe('Files', () => {
+    let app: INestApplication;
+    let server: any;
+
+    const jwtGuard = JwtAuthGuardMock;
+    const uploadFolder = `${path}/uploads`;
+    const filePictureMock = {
+        value: {
+            fieldname: 'file',
+            originalname: 'IMG_8951.HEIC',
+            encoding: '7bit',
+            mimetype: 'image/heic',
+            buffer: {
+                type: 'Buffer',
+                data: '',
+            },
+            size: 584214,
+        },
+    };
+    const serviceMock = {
+        saveFile: jest.fn().mockResolvedValue({
+            url: `${uploadFolder}/${filePictureMock.value.originalname}`,
+            name: filePictureMock.value.originalname,
+        }),
+    };
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
+            providers: [{ provide: FilesService, useValue: serviceMock }],
             controllers: [FilesController],
-        }).compile();
+        })
+            .overrideGuard(JwtAuthGuard)
+            .useValue(jwtGuard)
+            .compile();
 
-        controller = module.get<FilesController>(FilesController);
+        app = await createTestModule(module);
+        server = app.getHttpServer();
     });
 
-    it('should be defined', () => {
-        expect(controller).toBeDefined();
+    // it('should upload file successfuly', () => {
+    //     return request(server)
+    //         .post('/files/upload')
+    //         .send(filePictureMock)
+    //         .expect(HttpStatus.OK);
+    // });
+
+    it(`with error not authorized`, () => {
+        jwtGuard.canActivate.mockReturnValueOnce(false);
+
+        return request(server)
+            .post('/files/upload')
+            .send(filePictureMock)
+            .expect(HttpStatus.FORBIDDEN);
     });
 });
--- src/files/files.service.spec.ts
+++ src/files/files.service.spec.ts
@@ -1,44 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FilesService } from './files.service';
-import { createTestModule } from '../../test/utils/create-test-module';
-import { HttpStatus, INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { JwtAuthGuardMock } from '../shared/guards/jwt-auth.guard.mock';
-import { JwtAuthGuard } from '../shared/guards/jwt-auth.guard';
 
 describe('FilesService', () => {
     let service: FilesService;
-    let app: INestApplication;
-    let server: any;
-    const jwtGuard = JwtAuthGuardMock;
-    const serviceMock = {
-        upload: jest.fn().mockResolvedValue({}),
-    };
-
-    const filePictureMock = {
-        value: {
-            fieldname: 'file',
-            originalname: 'IMG_8951.HEIC',
-            encoding: '7bit',
-            mimetype: 'image/heic',
-            buffer: {
-                type: 'Buffer',
-                data: '',
-            },
-            size: 584214,
-        },
-    };
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [{ provide: FilesService, useValue: serviceMock }],
-        })
-            .overrideGuard(JwtAuthGuard)
-            .useValue(jwtGuard)
-            .compile();
+            providers: [FilesService],
+        }).compile();
 
-        app = await createTestModule(module);
-        server = app.getHttpServer();
         service = module.get<FilesService>(FilesService);
     });
 
@@ -46,10 +16,8 @@ describe('FilesService', () => {
         expect(service).toBeDefined();
     });
 
-    xit('should upload file successfuly', () => {
-        return request(server)
-            .post('/files/upload')
-            .send(filePictureMock)
-            .expect(HttpStatus.OK);
-    });
+    // xit('saveFile', async () => {
+    //     await expect(service.saveFile(filePictureMock as Express.Multer.File))
+    //         .rejects.toThrow('');
+    // });
 });
